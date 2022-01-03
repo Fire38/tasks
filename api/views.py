24bfcf9eefeea2f3c2099c61f7a48fe1883e9aa3
Sablin Roman <sablin_1992@mail.ru>
2022-01-03 21:42:47 +0800

    поправил бэк

--- api/views.py
+++ api/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Rubric, RubricItem
 from .serializers import RubricSerializer, RubricItemSerializer
-from django.utils.decorators import method_decorator
 
 
 class GetRubric(APIView):
@@ -15,20 +11,11 @@ class GetRubric(APIView):
         serializer = RubricSerializer(rubrics, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        user = request.user
-        rubric = request.data
-        obj, created = Rubric.objects.get_or_create(name=rubric['data']['rubricName'],
-                                                    user=user
-                                                    )
-        return Response(status=status.HTTP_200_OK)
-
 
 class AddRubric(APIView):
     def post(self, request):
         user = request.user
         rubric = request.data
-        print(request.data)
         obj, created = Rubric.objects.get_or_create(name=rubric['rubricName'],
                                                     user=user
                                                     )
@@ -38,7 +25,6 @@ class AddRubric(APIView):
             return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
 
 
-#@method_decorator(csrf_exempt, name='post')
 class GetRubricItem(APIView):
     def get(self, request, task_type='all', filter_id=''):
         print(task_type, type(filter_id), filter_id)
@@ -61,9 +47,7 @@ class GetRubricItem(APIView):
 
 
 class AddRubricItem(APIView):
-
     def post(self, request):
-        print(request.data)
         user = request.user
         rubric_item = request.data
         rubric = Rubric.objects.get(id=rubric_item['data']['selectedRubric'])
@@ -75,27 +59,18 @@ class AddRubricItem(APIView):
         ).exists():
             return Response(status=status.HTTP_501_NOT_IMPLEMENTED)
         else:
-
             rubric_item = RubricItem(name=rubric_item['data']['title'],
-                                                           description=rubric_item['data']['description'],
-                                                           rubric=rubric,
+                                       description=rubric_item['data']['description'],
+                                       rubric=rubric,
                                      )
             rubric_item.save()
             rubric_item.user.add(user)
-
-            print(rubric_item)
-            print('Прилетел post')
             return Response(status=status.HTTP_201_CREATED)
 
     def put(self, request):
-        print(request.data, request.user)
         task_id = request.data['data']['id']
         task_status = request.data['data']['done']
         task = RubricItem.objects.get(user=request.user, id=task_id)
         task.done = task_status
         task.save()
-
-
         return Response(status=status.HTTP_200_OK)
-
-
