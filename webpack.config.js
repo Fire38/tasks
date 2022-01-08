fc51884bc41b1b454ac281b42533c1da50f8039a
Roman <sablin_1992@mail.ru>
2022-01-08 21:19:50 +0800

    123

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
-                }
+                },
+                options: { presets: ['@babel/env','@babel/preset-react'] },
             },
             {
                 test: /\.css$/,
