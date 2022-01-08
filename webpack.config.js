6dc4f86636ca83910ce619ba020f721f6355208c
Roman <sablin_1992@mail.ru>
2022-01-08 20:31:43 +0800

    123

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jpe?g|gif|woff|woff2|eot|ttf|svg)$/,
+                
+                    loader: 'url-loader'
+                
+            }
+        ]
+    },
+};
